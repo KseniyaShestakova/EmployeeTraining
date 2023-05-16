deedec9eeaf3c11a2125ae5c07e4917606e9c806
shestakova ksenija <shestakova.ko@phystech.edu>
2023-05-16 10:33:08 +0000

    Update scripts/triggers.sql

--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -27,5 +27,42 @@ INSERT INTO person VALUES
 
 SELECT * FROM instructor
 
-DROP SEQUENCE IF EXISTS person_id_seq;
-CREATE SEQUENCE person_id_seq START 11;
+DROP SEQUENCE IF EXISTS instr_id_seq;
+CREATE SEQUENCE instr_id_seq START 11;
+
+CREATE OR REPLACE FUNCTION process_instructor_insert()
+RETURNS TRIGGER AS
+$$
+BEGIN
+	---исправляем дату начала, чтобы не было добавлений "задним числом"
+	UPDATE instructor SET starttime=CURRENT_TIMESTAMP
+	WHERE instr_id = NEW.instr_id AND starttime < CURRENT_TIMESTAMP;
+	
+	---обнуляем предыдущую запись про того же человека
+	UPDATE instructor SET endtime=CURRENT_TIMESTAMP, is_current=0
+	WHERE person_id = NEW.person_id AND NOT(instr_id = NEW.instr_id)
+		AND (endtime > CURRENT_TIMESTAMP OR endtime IS NULL);
+	
+	---проставляем статус актуальности
+	UPDATE instructor SET is_current=1
+	WHERE instr_id = NEW.instr_id OR instr_id IS NULL;
+
+	---обновляем id записи
+	UPDATE instructor SET instr_id=nextval('instr_id_seq')
+	WHERE instr_id = NEW.instr_id OR instr_id IS NULL;
+	
+	RETURN NULL;
+END;
+$$ language plpgsql;
+
+CREATE OR REPLACE TRIGGER instructor_insert_trigger
+AFTER INSERT ON instructor
+FOR EACH ROW
+EXECUTE FUNCTION process_instructor_insert();
+
+---проверка того, что это работает, в т.ч. с множественным INSERT
+INSERT INTO instructor VALUES
+	(40, 5, 6, 'преподаватель английской литературы', 'ДИЯ', '2024-04-20', NULL, 0),
+	(50, 0, 0, 'ответственный за информационную безопасность', 'IT-отдел', '2023-04-20', NULL, 0);
+	
+	
