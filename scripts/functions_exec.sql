7046f35523c152d5d5acaf5cabf5c5fd6b81cf2c
shestakova ksenija <shestakova.ko@phystech.edu>
2023-05-16 21:07:40 +0000

    add function execution

---
+++ scripts/functions_exec.sql
@@ -0,0 +1,8 @@
+SELECT * FROM get_program_info('Продвинутая финансовая отчетность');
+SELECT * FROM get_program_info('Бухгалтерский учет в строительных организациях');
+SELECT * FROM get_program_info('Успешный классный руководитель');
+SELECT * FROM get_program_info('Несуществующая программа');
+SELECT get_actual_contracts('Аленькин Петр Валерьевич');
+SELECT get_actual_contracts('Штрих Инна Игнатовна');
+SELECT get_actual_contracts('Луговой Павел Павлович');
+SELECT get_actual_contracts('Несуществующий человек');
