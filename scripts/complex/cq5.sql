40bb6ca158a455e7fc25404d0ab60d6d544e6be9
shestakova ksenija <shestakova.ko@phystech.edu>
2023-05-17 06:39:36 +0000

    Update scripts/complex/cq5.sql

--- scripts/complex/cq5.sql
+++ scripts/complex/cq5.sql
@@ -1,6 +1,8 @@
-SELECT person_id, company_id, starttime,
+SELECT person_id, company_id, starttime, specialization,
 	LEAD(company_id, 1, NULL) OVER(PARTITION BY person_id ORDER BY starttime)
 		AS next_workplace,
+	LEAD(specialization, 1, NULL) OVER(PARTITION BY person_id ORDER BY starttime)
+		AS next_spec,
 	LEAD(starttime, 1, NULL) OVER(PARTITION BY person_id ORDER BY starttime)
 		AS next_worktime
 FROM instructor;
