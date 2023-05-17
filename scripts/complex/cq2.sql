634f0aeaf46b89931c55120fdb0bebe8a9d2334c
shestakova ksenija <shestakova.ko@phystech.edu>
2023-05-17 06:31:24 +0000

    Update scripts/complex/cq2.sql

--- scripts/complex/cq2.sql
+++ scripts/complex/cq2.sql
@@ -3,9 +3,9 @@ SELECT contract_id, sum,
 		AVG(sum) OVER(PARTITION BY contract_id) As avg_sum
 FROM payment
 )
-SELECT contract_id, sum, avg_sum,
+SELECT contract_id, sum, ROUND(avg_sum, 2),
 	CASE
-	WHEN sum < avg_sum THEN 'дешевле среднего'
+	WHEN sum < ROUND(avg_sum, 2) THEN 'дешевле среднего'
 	ELSE 'не дешевле среднего'
 	END
 	AS relative_sum
