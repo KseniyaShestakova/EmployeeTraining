f5647cf2b4eb81b8ecf6bf18954f72ee129e6ac7
shestakova ksenija <shestakova.ko@phystech.edu>
2023-05-17 06:26:55 +0000

    Update scripts/complex/cq1.sql

--- scripts/complex/cq1.sql
+++ scripts/complex/cq1.sql
@@ -1,5 +1,5 @@
 SELECT  contract_id,
 		COUNT(*) AS pay_num,
-		SUM(payer_ref) AS payer_ref_sum
+		SUM(payer) AS payer_ref_sum
 	FROM payment GROUP BY contract_id
-	HAVING COUNT(*) >= 1 AND SUM(payer_ref)=COUNT(*);
+	HAVING COUNT(*) >= 1 AND SUM(payer)=COUNT(*);
